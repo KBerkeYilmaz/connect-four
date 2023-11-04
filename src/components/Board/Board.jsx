07001ad2993af34db36e26391d4f086a654fed54
Kutalmış Berke Yılmaz <kberkeyilmaz@gmail.com>
2023-11-04 03:25:09 +0300

    Refactoring

---
+++ src/components/Board/Board.jsx
@@ -0,0 +1,145 @@
+import { useState } from "react";
+import Holes from "../UI/Holes";
+import Container from "../UI/Container";
+import PlayerCard from "../UI/PlayerCard";
+import BottomBanner from "../UI/BottomBanner";
+
+const emptyBoard = () => {
+  const rows = 6;
+  const columns = 7;
+  const board = [];
+  for (let i = 0; i < rows; i++) {
+    const row = Array(columns).fill(null);
+    board.push(row);
+  }
+  return board;
+};
+
+const checkForWinner = (board) => {
+  const directions = [
+    { x: 0, y: 1 }, // vertical
+    { x: 1, y: 0 }, // horizontal
+    { x: 1, y: 1 }, // diagonal left-to-right
+    { x: 1, y: -1 }, // diagonal right-to-left
+  ];
+
+  for (let y = 0; y < board.length; y++) {
+    for (let x = 0; x < board[y].length; x++) {
+      for (let dir of directions) {
+        if (checkDirection(board, x, y, dir)) {
+          return board[y][x];
+        }
+      }
+    }
+  }
+  return null;
+};
+
+const checkDirection = (board, startX, startY, direction) => {
+  const targetPlayer = board[startY][startX];
+  if (!targetPlayer) return false;
+
+  for (let step = 1; step < 4; step++) {
+    const x = startX + direction.x * step;
+    const y = startY + direction.y * step;
+    if (
+      y < 0 ||
+      y >= board.length ||
+      x < 0 ||
+      x >= board[y].length ||
+      board[y][x] !== targetPlayer
+    ) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const Board = () => {
+  const [board, setBoard] = useState(emptyBoard());
+  const [hasChips, setHasChips] = useState(false);
+  const [playerTurn, setPlayerTurn] = useState(1);
+  const [tileDropped, setTileDropped] = useState(false);
+  const [activeColumn, setActiveColumn] = useState(null);
+  const [winner, setWinner] = useState(null);
+  const [score, setScore] = useState(0);
+
+  const renderBoard = () => {
+    // Iterate over each row in the board
+    return board.map((row, rowIndex) => {
+      // For each row, iterate over each cell
+      return row.map((cell, columnIndex) => {
+        // Render the Holes component (or a different component based on cell value)
+        return (
+          <Holes
+            key={`r${rowIndex}c${columnIndex}`}
+            id={`Cell r${rowIndex}c${columnIndex}`}
+            turn={playerTurn}
+            changeTurn={changeTurn}
+            board={board}
+            hasChips={board[rowIndex][columnIndex]}
+            setHasChips={setBoard}
+            rowIndex={rowIndex}
+            columnIndex={columnIndex}
+          />
+        );
+      });
+    });
+  };
+
+  const changeTurn = () => {
+    const winner = checkForWinner(board);
+    if (winner) {
+      setWinner(winner);
+      // Maybe stop the game, show an alert, or handle it some other way.
+      return; // Do not change the turn if there's a winner.
+    }
+
+    if (playerTurn === 1) {
+      setPlayerTurn(2);
+    } else {
+      setPlayerTurn(1);
+    }
+  };
+
+  return (
+    <>
+      <Container
+        className=" 
+        w-2/5 
+                      h-4/6 
+                      absolute 
+                      top-1/2 
+                      left-1/2 
+                      transform 
+                      -translate-x-1/2 
+                      -translate-y-1/2
+                      grid
+                      grid-cols-7
+                      pt-4 
+                      px-2
+                      pb-12
+                      gap-y-2"
+      >
+        {renderBoard()}
+      </Container>
+
+      <PlayerCard
+        key={0}
+        playerNo={1}
+        playerScore={0}
+        position="left"
+      />
+      <PlayerCard
+        key={1}
+        playerNo={2}
+        playerScore={0}
+        position="right"
+      />
+
+      <BottomBanner playerTurn={playerTurn} />
+    </>
+  );
+};
+
+export default Board;
--- src/components/UI/BottomBanner.jsx
+++ src/components/UI/BottomBanner.jsx
@@ -1,27 +1,25 @@
 const BottomBanner = (props) => {
   return (
     <div className="relative left-0 right-0 top-[80%] h-1/5 w-full bg-violet-800 rounded-t-[4rem] ">
-      <div className="z-30 absolute -top-3/4 right-0 left-0 m-auto h-full w-52">
-        <svg
-          width="15.3rem"
-          height="12.5rem"
-          viewBox="1 0 13 0.1"
-          fill="#f87171"
+      <div className="z-30 absolute -top-3/4 right-0 left-0 m-auto h-full w-60">
+        {/* <svg
+          width="250"
+          height="250"
+          viewBox="0 0 200 100"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            d="M1,2 L6.5,0 L12,2" 
+            d="M 100,50 L 10,90 L 190,90 Z"
+            fill="#f87171"
             stroke="black"
-            strokeWidth="0.15"
-            fill="#f87171" 
-            strokeLinecap="round"
-            strokeLinejoin="round"
+            strokeWidth="2"
           />
-        </svg>
-      </div>
-
-      <div className="z-30 absolute bottom-12 right-0 left-0 top-0 m-auto h-5/6 w-52 bg-red-400 border-l-4 border-r-4 border-b-[12px] border-solid border-black rounded-b-3xl flex flex-col justify-start items-center">
-        <p className="text-white text-3xl mt-2 font-mono font-extrabold">Player {props.playerTurn}'s Turn</p>
+        </svg> */}
+        <div className="z-30 absolute bottom-0 right-0 left-0 top-56 m-auto h-4/6 w-64 bg-red-400 border-l-4 border-r-4 border-t-4 border-b-[12px] border-solid border-black rounded-2xl flex flex-col justify-start items-center">
+          <p className="text-white text-2xl mt-2 font-mono font-extrabold">
+            Player {props.playerTurn}'s Turn
+          </p>
+        </div>
       </div>
     </div>
   );
